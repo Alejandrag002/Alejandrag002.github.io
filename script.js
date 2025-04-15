5b9081f8b9c0e51b65ad093528db5b2e1660d98d
Alejandrag002 <54290129+Alejandrag002@users.noreply.github.com>
2025-04-15 15:40:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -105,6 +105,16 @@ document.addEventListener("DOMContentLoaded", function () {
                 showTab(tabId);
             }
         });
+
+        const hamburger = document.getElementById("hamburger");
+        const navLinks = document.getElementById("navLinks");
+
+        if (hamburger && navLinks) {
+        hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+}
+
     });
 
     // Image hover 
@@ -165,14 +175,9 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    // Hamburger menu toggle
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("navLinks");
-
-if (hamburger && navLinks) {
-    hamburger.addEventListener("click", () => {
-        navLinks.classList.toggle("active");
-    });
-}
+    
 
 });
+
+
+
